9206da2a5651728662b268a79adcd91b0586d415
LemWalk <lemuel.walkinshaw@netmatters-scs.com>
2023-09-08 21:17:21 +0100

    *validator fixes*

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+//========= Burger menu & Side menu functions =========
+
+const burgerMenu = document.querySelector('.burger-menu');
+const sideMenu = document.querySelector('.side-menu');
+const overlay = document.querySelector('.overlay');
+const containerOuter = document.querySelector('.container-outer');
+
+burgerMenu.addEventListener('click', () => {
+burgerMenu.classList.toggle('active');
+sideMenu.classList.toggle('active');
+containerOuter.classList.toggle('active');
+overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    sideMenu.classList.toggle('active');
+    containerOuter.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
