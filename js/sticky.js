24a5c5e0a13f9f5d1ff24d914aa093a3e9452267
LemWalk <lemuel.walkinshaw@netmatters-scs.com>
2023-11-15 16:49:25 +0000

    *nav doesn't ease up*

--- js/sticky.js
+++ js/sticky.js
@@ -1,53 +1,19 @@
 
-// Get the navbar
+var prevScrollpos = window.scrollY;
 var navbar = document.querySelector(".nav-container");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Set the minimum scroll height
-var minScrollHeight = 0;
-
-// When the user scrolls down, hide the navbar
-var prevScrollpos = window.scrollY;
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
-
-  if (prevScrollpos > currentScrollPos && currentScrollPos > minScrollHeight) {
-    navbar.classList.add("sticky");    
+  if (prevScrollpos > currentScrollPos && currentScrollPos > sticky) {
+    navbar.classList.add("sticky");
+    navbar.style.transition = "top 0.5s ease-in-out";
     navbar.style.top = "0";
-  } else {
-    document.querySelector('.sticky').style.transition = "top 0.5s ease-out"; 
+  } else {    
+    navbar.classList.remove("sticky");
+    navbar.style.transition = "top 0.5s ease-in-out";
     navbar.style.top = "-220px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
 
-//====================================
-
-// // Get the navbar
-// var navbar = document.querySelector(".nav-container");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Set the minimum scroll height
-// var minScrollHeight = 0;
-
-// // When the user scrolls down, hide the navbar
-// var prevScrollpos = window.scrollY;
-// window.onscroll = function() {
-//   var currentScrollPos = window.scrollY;
-
-//   if (prevScrollpos > currentScrollPos && currentScrollPos > minScrollHeight) {
-//     navbar.classList.add("sticky");    
-//     navbar.style.top = "0";
-//   } else if (currentScrollPos <= minScrollHeight) { // Add this condition
-//     navbar.classList.remove("sticky");
-//     navbar.style.top = "0";
-//   } else {
-//     document.querySelector('.sticky').style.transition = "top 0.5s ease-out"; 
-//     navbar.style.top = "-220px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// };
