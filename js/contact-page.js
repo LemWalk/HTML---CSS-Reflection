7bd9e48f75dd99112767f0d76c188e1df534e149
LemWalk <lemuel.walkinshaw@netmatters-scs.com>
2024-02-01 10:57:05 +0000

    *javaScript fix *

--- js/contact-page.js
+++ js/contact-page.js
@@ -12,9 +12,8 @@ function display_details() {
 
 accordion.addEventListener('click', (display_details));
 
-// ===== Success Message Close =====
+// ===== Success and Error Message Close =====
 
-// let successBox = document.getElementsByClassName('submit_message_box');
 
 const btn_success_close = document.querySelector('.submit_message_close');
 const btn_error_close = document.querySelector('.submit_message_close_error');
@@ -22,14 +21,21 @@ const successMessage = document.querySelector('.submit_message_box');
 const errorMessage = document.querySelector('.submit-failed');
 
 
+if(btn_success_close){
+    btn_success_close.addEventListener('click', (closeSuccessMessage));
+}
+
+if( btn_error_close){
+    btn_error_close.addEventListener('click', (closeSuccessMessage));  
+}
+
 function closeSuccessMessage() {
     successMessage.style.display = 'none';
 }
+
 function closeErrorMessage() {
     errorMessage.style.display = 'none';
 }
-btn_success_close.addEventListener('click', (closeSuccessMessage));
-btn_error_close.addEventListener('click', (closeErrorMessage));
 
 
 
